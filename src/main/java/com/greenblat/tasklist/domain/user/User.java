55c328b7e5964a1374ef027178e7dcb42f82feb2
Alex Zhuravlev <leftemail17@mail.ru>
2023-10-15 16:51:47 +0300

    create models

---
+++ src/main/java/com/greenblat/tasklist/domain/user/User.java
@@ -0,0 +1,20 @@
+package com.greenblat.tasklist.domain.user;
+
+import com.greenblat.tasklist.domain.task.Task;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Set;
+
+@Data
+public class User {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    private String passwordConfirmation;
+    private Set<Role> roles;
+    private List<Task> tasks;
+
+}
