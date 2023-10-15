d5d2b59f848d0953d8bbfad7c25d233363551643
Alex Zhuravlev <leftemail17@mail.ru>
2023-10-15 17:03:30 +0300

    create repositories

---
+++ src/main/java/com/greenblat/tasklist/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,47 @@
+package com.greenblat.tasklist.repository.impl;
+
+import com.greenblat.tasklist.domain.user.Role;
+import com.greenblat.tasklist.domain.user.User;
+import com.greenblat.tasklist.repository.UserRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void create(User user) {
+
+    }
+
+    @Override
+    public void insertUserRole(Long userId, Role role) {
+
+    }
+
+    @Override
+    public boolean isTaskOwner(Long userId, Long taskId) {
+        return false;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
