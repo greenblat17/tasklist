303a04a932f470de014cf6093a572ec6c3bf1a26
Alex Zhuravlev <leftemail17@mail.ru>
2023-10-16 14:48:24 +0300

    implement UserService

--- src/main/java/com/greenblat/tasklist/service/impl/UserServiceImpl.java
+++ src/main/java/com/greenblat/tasklist/service/impl/UserServiceImpl.java
@@ -1,39 +1,77 @@
 package com.greenblat.tasklist.service.impl;
 
+import com.greenblat.tasklist.domain.exception.ResourceNotFoundException;
+import com.greenblat.tasklist.domain.user.Role;
 import com.greenblat.tasklist.domain.user.User;
+import com.greenblat.tasklist.repository.UserRepository;
 import com.greenblat.tasklist.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User getById(Long id) {
-        return null;
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
     public User getByUsername(String username) {
-        return null;
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 
     @Override
+    @Transactional
     public User update(User user) {
-        return null;
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.update(user);
+        return user;
     }
 
     @Override
+    @Transactional
     public User create(User user) {
-        return null;
+        userRepository.findByUsername(user.getUsername())
+                .ifPresent(e -> {
+                    throw new IllegalStateException("User already exists");
+                });
+
+        var password = user.getPassword();
+        if (!password.equals(user.getPasswordConfirmation())) {
+             throw new IllegalStateException("Password and password confirmation do not match");
+        }
+        user.setPassword(passwordEncoder.encode(password));
+        userRepository.create(user);
+
+        var usersRole = Role.ROLE_USER;
+        userRepository.insertUserRole(user.getId(), usersRole);
+
+        var roles = Set.of(usersRole);
+        user.setRoles(roles);
+
+        return user;
     }
 
     @Override
     public boolean isTaskOwner(Long userId, Long taskId) {
-        return false;
+        return userRepository.isTaskOwner(userId, taskId);
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
-
+        userRepository.delete(id);
     }
 }
