16892f5d6b61d3f22ab6f0a20eed8be31ffaf3a0
Alex Zhuravlev <leftemail17@mail.ru>
2023-10-16 14:34:33 +0300

    implement TaskService

--- src/main/java/com/greenblat/tasklist/service/impl/TaskServiceImpl.java
+++ src/main/java/com/greenblat/tasklist/service/impl/TaskServiceImpl.java
@@ -1,36 +1,55 @@
 package com.greenblat.tasklist.service.impl;
 
+import com.greenblat.tasklist.domain.exception.ResourceNotFoundException;
+import com.greenblat.tasklist.domain.task.Status;
 import com.greenblat.tasklist.domain.task.Task;
+import com.greenblat.tasklist.repository.TaskRepository;
 import com.greenblat.tasklist.service.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
 
     @Override
     public Task getById(Long id) {
-        return null;
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task not found"));
     }
 
     @Override
     public List<Task> getAllByUserId(Long userId) {
-        return null;
+        return taskRepository.findAllByUserId(userId);
     }
 
     @Override
+    @Transactional
     public Task update(Task task) {
-        return null;
+        if (task.getStatus() == null) {
+            task.setStatus(Status.TODO);
+        }
+        taskRepository.update(task);
+        return task;
     }
 
     @Override
+    @Transactional
     public Task create(Task task, Long userId) {
-        return null;
+        task.setStatus(Status.TODO);
+        taskRepository.create(task);
+        taskRepository.assignToUserById(task.getId(), userId);
+        return task;
     }
 
     @Override
     public void delete(Long id) {
-
+        taskRepository.delete(id);
     }
 }
