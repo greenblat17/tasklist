5a9922923b9ea8f7f13807ea092a0856a827f511
Alex Zhuravlev <leftemail17@mail.ru>
2023-11-14 18:43:10 +0300

    make entity serializable to caching

--- src/main/java/com/greenblat/tasklist/domain/user/User.java
+++ src/main/java/com/greenblat/tasklist/domain/user/User.java
@@ -3,11 +3,12 @@ package com.greenblat.tasklist.domain.user;
 import com.greenblat.tasklist.domain.task.Task;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Set;
 
 @Data
-public class User {
+public class User implements Serializable {
 
     private Long id;
     private String name;
