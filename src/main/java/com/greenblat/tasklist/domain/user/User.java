197354d0c5c42f718516879f95f1b78b4ffbd720
Alex Zhuravlev <leftemail17@mail.ru>
2023-11-16 13:13:03 +0300

    add jpa entity

--- src/main/java/com/greenblat/tasklist/domain/user/User.java
+++ src/main/java/com/greenblat/tasklist/domain/user/User.java
@@ -1,21 +1,43 @@
 package com.greenblat.tasklist.domain.user;
 
 import com.greenblat.tasklist.domain.task.Task;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.List;
 import java.util.Set;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String username;
+
     private String password;
+
+    @Transient
     private String passwordConfirmation;
+
+    @Column(name = "role")
+    @ElementCollection
+    @CollectionTable(name = "users_roles")
+    @Enumerated(value = EnumType.STRING)
     private Set<Role> roles;
+
+    @CollectionTable(name = "users_tasks")
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "task_id")
     private List<Task> tasks;
+
 
 }
